77990530714b1594d7c069d19360120d46df7941
Dulaul <dennis-ionut.radoi@s.unibuc.ro>
2026-08-04 11:16:57 +0300

    facut pagina colegi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Sidebar from "@/components/Sidebar"
+import CompanyPage from "@/pages/CompanyPage"
 import HomePage from "@/pages/HomePage"
 import SeatsPage from "@/pages/SeatsPage"
 import "./App.css"
@@ -11,6 +12,7 @@ export default function App() {
       <main className="app-main">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/companie" element={<CompanyPage />} />
           <Route path="/seats" element={<SeatsPage />} />
         </Routes>
       </main>
